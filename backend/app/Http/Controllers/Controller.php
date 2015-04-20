4dfad16ee6045c02754c6920ea004c9508030e54
Vishwajeet Vatharkar <vishwajeetvatharkar@gmail.com>
2015-04-20 16:31:18 +0530

    Reusable response method created - respond in Controller

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -8,4 +8,36 @@ abstract class Controller extends BaseController {
 
 	use DispatchesCommands, ValidatesRequests;
 
+
+    public function generateResponse($status, $message, $body)
+    {
+        return response([
+            'header' => array(
+                'status' => $status,
+                'message' => $message
+            ),
+            'body' =>
+                $body
+        ], 200)
+            ->header('Content-Type', 'json');
+
+    }
+
+    public function respond($flag, $message, $data, $errors)
+    {
+        if ($flag) {
+            return $this->generateResponse(
+                'success',
+                $message,
+                $data
+            );
+        } else {
+            return $this->generateResponse(
+                'error',
+                $message,
+                $errors
+            );
+        }
+    }
+
 }
