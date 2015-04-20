ff20c9f06a3acd514f2e8daca20e2c7fec44718a
Vishwajeet Vatharkar <vishwajeetvatharkar@gmail.com>
2015-04-20 23:37:42 +0530

    Save suggestion backend done.

--- backend/app/Http/Controllers/SuggestionController.php
+++ backend/app/Http/Controllers/SuggestionController.php
@@ -3,7 +3,7 @@
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-use Illuminate\Http\Request;
+use Request;
 
 class SuggestionController extends Controller {
 
@@ -34,7 +34,48 @@ class SuggestionController extends Controller {
 	 */
 	public function store()
 	{
-		//
+	    $citizenInput = Request::input('citizen');
+
+        $suggestionInput = Request::input('suggestion');
+
+        $instanceId = 1;
+
+        $user = User::where('email','=',$citizenInput['email']);
+        if(isset($user))
+        {
+            $user->name = $citizenInput['name'];
+            $user->mobile = $citizenInput['mobile'];
+            $user->instance_id = $instanceId;
+            $userSaveSuccess = $user->save();
+        }
+        else
+        {
+            $user = new User;
+            $user->email = $citizenInput['email'];
+            $user->name = $citizenInput['name'];
+            $user->mobile = $citizenInput['mobile'];
+            $user->instance_id = $instanceId;
+            $userSaveSuccess = $user->save();
+        }
+
+        if(isset($userSaveSuccess))
+        {
+            $suggestion = new Suggestion;
+            $suggestion->instance_id = $instanceId;
+            $suggestion->city_function_id = $suggestionInput['work']['id'];
+            $suggestion->zone_division_id = $suggestionInput['division']['id'];
+            $suggestion->suggestion = $suggestionInput['suggestion'];
+            $suggestion->status = 'citizen_submitted';
+            $suggestionSaveSuccess = $suggestion->save();
+        }
+        else
+        {
+            return $this->respond(null,null,'Failed to save user',null,'User saving error');
+
+        }
+
+        return $this->respond($suggestionSaveSuccess,'Suggestion saved successfully','could not save suggestion',$suggestion,'suggestion error');
+
 	}
 
 	/**
