de3dfc316718e4f57044d7ac5187a8b1f6fed407
Vishwajeet Vatharkar <vishwajeetvatharkar@gmail.com>
2015-04-20 15:12:52 +0530

    Zones table migration added.

---
+++ backend/database/migrations/2015_04_20_093458_create_zones_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateZonesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('zones', function(Blueprint $table)
+		{
+			$table->increments('id');
+            $table->integer('city_id')->unsigned();
+            $table->foreign('city_id')->references('id')->on('cities');
+            $table->string('name');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('zones');
+	}
+
+}
