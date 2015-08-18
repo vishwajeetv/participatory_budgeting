b574e815b8b267553aff514339c9c32f74b88bba
Vishwajeet Vatharkar <vishwajeetvatharkar@gmail.com>
2015-08-19 04:24:49 +0530

    email sending fixed

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Support\Facades\Mail;
 
 abstract class Controller extends BaseController
 {
