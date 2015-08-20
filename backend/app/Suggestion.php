44ee2af6b6f872c4f79e3ed81fea44d5e8d44b06
Vishwajeet Vatharkar <vishwajeetvatharkar@gmail.com>
2015-08-20 19:45:40 +0530

    suggestion model fillable fixed

--- backend/app/Suggestion.php
+++ backend/app/Suggestion.php
@@ -18,7 +18,7 @@ class Suggestion extends Model {
      *
      * @var array
      */
-    protected $fillable = ['id','city_id'];
+    protected $fillable = ['id','city_id','suggestion','instance_id','zone_division_id','city_function_id','area','status'];
 
     /**
      * The attributes excluded from the model's JSON form.
