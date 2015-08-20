700248c7d0781dea9d24968b23b10462c2973575
Vishwajeet Vatharkar <vishwajeetvatharkar@gmail.com>
2015-08-21 00:03:29 +0530

    save suggestion request added

---
+++ backend/app/Http/Requests/SaveSuggestionRequest.php
@@ -0,0 +1,34 @@
+<?php namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class SaveSuggestionRequest extends Request {
+
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return true;
+	}
+
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+        return [
+
+            'work_id' => 'required',
+            'division_id' => 'required',
+            'area' => 'required',
+            'suggestion' => 'required',
+            'work_purpose' => 'required',
+        ];
+	}
+
+}
