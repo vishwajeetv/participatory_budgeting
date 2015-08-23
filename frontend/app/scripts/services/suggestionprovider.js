6b7b9af5be9e091f4debe5c7d3802c559bce7911
Vishwajeet Vatharkar <vishwajeetvatharkar@gmail.com>
2015-08-24 01:54:09 +0530

    get and save suggestion methods created in suggestion provider

--- frontend/app/scripts/services/suggestionprovider.js
+++ frontend/app/scripts/services/suggestionprovider.js
@@ -8,6 +8,47 @@
  * Service in the frontendApp.
  */
 angular.module('frontendApp')
-  .service('SuggestionProvider', function () {
-    // AngularJS will instantiate a singleton by calling "new" on this function
+  .service('SuggestionProvider', function ( $q, $http , Restangular , SERVER_URL ) {
+
+      this.getSuggestionsByUser = function (instanceId , userId) {
+        var url = SERVER_URL + 'suggestion/show-by-user?user_id='+userId+'&instance_id='+instanceId;
+        var deferred = $q.defer();
+        $http.get(url).
+            success(function (data, status, headers, config) {
+              if(data.header.status == 'success')
+              {
+                deferred.resolve(data.body);
+              }
+              else
+              {
+                deferred.reject();
+              }
+            }).
+            error(function (data, status, headers, config) {
+              console.log("could not get person type id");
+              deferred.reject();
+            });
+        return deferred.promise;
+      };
+
+        this.saveSuggestion = function(suggestion){
+
+            var url = SERVER_URL + 'suggestion';
+            var deferred = $q.defer();
+
+            $http.post(url, suggestion).
+                success(function (response, status) {
+                    if(response.header.status == 'success')
+                        deferred.resolve(response);
+                    else
+                        deferred.reject(response);
+                }).
+                error(function (data, status) {
+                    console.log(data);
+                    deferred.reject(data);
+                    console.log('could not store suggestion');
+                });
+            return deferred.promise;
+
+        }
   });
