fe521e7a852186e5f886e5968d9b9f04ab748a38
Vishwajeet Vatharkar <vishwajeetvatharkar@gmail.com>
2015-08-28 00:00:42 +0530

    directive created for menu.

---
+++ frontend/app/scripts/directives/menu.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name frontendApp.directive:menu
+ * @description
+ * # menu
+ */
+angular.module('frontendApp')
+  .directive('menu', function () {
+    return {
+      templateUrl: 'views/menu.html',
+      restrict: 'E',
+      scope:
+      {
+        active : '@'
+      },
+      controller : function ($scope) {
+
+        $scope.activeClass = function(classToCheck)
+        {
+          if($scope.active == classToCheck )
+          {
+            return 'md-warn';
+          }
+          else
+          {
+            return 'md-primary md-hue-1 md-whiteframe-z1';
+          }
+        }
+      }
+    };
+  });
