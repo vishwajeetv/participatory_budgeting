b73e5a80dc9eb89579056fdddddfca0731f269e9
Vishwajeet Vatharkar <vishwajeetvatharkar@gmail.com>
2015-09-12 01:39:47 +0530

    unused dependancies removed.

--- frontend/test/karma.conf.js
+++ frontend/test/karma.conf.js
@@ -21,13 +21,11 @@ module.exports = function(config) {
       // bower:js
       'bower_components/angular/angular.js',
       'bower_components/angular-animate/angular-animate.js',
-      'bower_components/angular-cookies/angular-cookies.js',
       'bower_components/angular-aria/angular-aria.js',
       'bower_components/angular-material/angular-material.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
-      'bower_components/cartodb.js/cartodb.js',
       'bower_components/lodash/lodash.js',
       'bower_components/restangular/dist/restangular.js',
       'bower_components/angular-mocks/angular-mocks.js',
