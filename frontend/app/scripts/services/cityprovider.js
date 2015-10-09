94dcfbd38ac08b2d0254d0f57b526d0f49925a84
Vishwajeet Vatharkar <vishwajeetvatharkar@gmail.com>
2015-10-09 23:02:51 +0530

    get works service added

--- frontend/app/scripts/services/cityprovider.js
+++ frontend/app/scripts/services/cityprovider.js
@@ -26,7 +26,29 @@ angular.module('frontendApp')
                 }).
                 error(function (data, status) {
                     deferred.reject(data);
-                    console.log('could not create user');
+                    console.log('could not get zones');
+                });
+            return deferred.promise;
+        }
+
+        this.getWorks = function()
+        {
+            var url = SERVER_URL + 'city/show-works';
+            var requestData = {
+                'instance_id' : INSTANCE_ID
+            };
+            var deferred = $q.defer();
+
+            $http.post(url, requestData).
+                success(function (response, status) {
+                    if(response.header.status == 'success')
+                        deferred.resolve(response);
+                    else
+                        deferred.reject(response);
+                }).
+                error(function (data, status) {
+                    deferred.reject(data);
+                    console.log('could not get works');
                 });
             return deferred.promise;
         }
